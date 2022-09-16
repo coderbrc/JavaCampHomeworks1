0039f9b2d1e2b74d5cf0f592f226598d81b9f26c
burçak kasap <burcak_kasap92@hotmail.com>
2022-09-16 22:56:10 +0300

    First commit

---
+++ stringsDemo/src/stringsDemo/Main.java
@@ -0,0 +1,37 @@
+package stringsDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String mesaj = "        Bugün hava çok güzel";
+		System.out.println(mesaj);
+		/*
+		System.out.println("Eleman Sayısı: " + mesaj.length());
+		System.out.println("5. Eleman: " + mesaj.charAt(4));
+		System.out.println(mesaj.concat("Yaşasın!"));
+		System.out.println(mesaj.startsWith("B"));
+		System.out.println(mesaj.endsWith("."));
+		
+		char[] karakter=new char[5];
+		mesaj.getChars(0, 4, karakter, 0);
+		System.out.println(karakter);
+		System.out.println(mesaj.indexOf('a'));
+		System.out.println(mesaj.lastIndexOf('a'));
+		*/
+		String yenimesaj=mesaj.replace(' ', '-');
+		System.out.println(yenimesaj);
+		System.out.println("---------------------");
+		System.out.println(mesaj.substring(5));
+		System.out.println("---------------------");
+		System.out.println(mesaj.substring(2,5));
+		System.out.println("---------------------");
+		for(String kelime:mesaj.split(" ")) {
+			System.out.println(kelime);
+		}
+		System.out.println(mesaj.toLowerCase());
+		System.out.println("---------------------");
+		System.out.println(mesaj.toUpperCase());
+		System.out.println("---------------------");
+		System.out.println(mesaj.trim());
+	}
+}
