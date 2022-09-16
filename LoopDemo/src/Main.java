8df00fe72ed27ca5eff063f9812ee60cf2c954b3
burçak kasap <burcak_kasap92@hotmail.com>
2022-09-16 22:51:53 +0300

    First commit

---
+++ LoopDemo/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("For döngüsü bitti.");
+		int i = 1;
+		while (i < 10) {
+			System.out.println(i);
+			i += 2;
+		}
+		System.out.println("While döngüsü bitti.");
+		int j = 1;
+		do {
+			System.out.println(j);
+			j += 2;
+		} while (j < 10);
+		System.out.println("Do-While döngüsü bitti.");
+
+	}
+}
